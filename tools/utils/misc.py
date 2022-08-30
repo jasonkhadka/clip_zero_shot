0af3002f62077a916bcc063ca7df3eb72e2484d0
jasonkhadka <jasonkhadka33@gmail.com>
2022-08-30 16:21:44 +0200

    cluster mean

--- tools/utils/misc.py
+++ tools/utils/misc.py
@@ -14,3 +14,19 @@ def embed_class_labels(
         text_features = model.encode_text(text_inputs)
 
     return text_features
+
+
+def compute_cluster_means(
+        image_embeddings,
+        image_labels,
+):
+    cluster_means = []
+    cluster_count = []
+    for idx in range(len(image_labels.unique())):
+        target_embeddings = image_embeddings[image_labels == idx]
+        cluster_means.append(target_embeddings.mean(dim=0, keepdim=True))
+        cluster_count.append(target_embeddings.shape[0])
+
+    cluster_means = torch.cat(cluster_means)
+    cluster_count = image_labels.unique(return_counts=True)[1]
+    return cluster_means, cluster_count
